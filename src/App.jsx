9ef3c69e1d7b611c6962d6fd437677650ea22903
samglz10 <samglz10@gmail.com>
2024-03-18 14:39:09 -0400

    changing css styles and react state changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,21 @@ const baseUrl = 'https://pokeapi.co/api/v2';
 
 function App() {
   const [pokemonData, setPokemonData] = useState(null);
-  const [pokemonMoves, setPokemonMoves] = useState(null);
   const [pokemonId, setPokemonId] = useState(1);
 
   useEffect(
     () => {
       async function getPokemon() {
+        if (pokemonId < 1) {
+          setPokemonId(1);
+        }
         const response = await fetch(`${baseUrl}/pokemon/${pokemonId}`, { method: 'GET' });
         // console.log(response.url);
         const pokemon = await response.json();
         setPokemonData(pokemon);
       }
-      async function getPokemonMoves() {
-        const moveResponse = await fetch(`${baseUrl}/move/${pokemonId}`, { method: 'GET' });
-        console.log(moveResponse.url);
-        const moves = await moveResponse.json();
-        setPokemonMoves(moves.name);
-      }
       setPokemonData(null);
       getPokemon();
-      setPokemonMoves(null);
-      getPokemonMoves();
     },
     [pokemonId],
   );
@@ -37,12 +31,7 @@ function App() {
   const incrementPokemonId = () => {
     setPokemonId((prevPokemonId) => prevPokemonId + 1);
   };
-  /*
-  const allPokemonMoves = () => {
-    for (const allMoves in pokemonData.moves) {
-      const allPokemonMoves = allMoves;
 
-  } */
   return (
 
     <div>
@@ -53,7 +42,20 @@ function App() {
           <div>
             <div className="pokemon-name-container">
               <img src={pokemonData.sprites.front_default} alt={`front facing ${pokemonData.name}`} />
-              <h2>{pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)}</h2>
+              <h2> 
+                {pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)}
+                {' '}
+              </h2>
+             {/**  <span>
+                {pokemonData.types[0].type.name}
+                {' '}
+                and
+                {' '}
+                {pokemonData.types[1].type.name}
+                {' '}
+                type
+              </span>
+              */}
             </div>
             <h3>Movesets</h3>
             <div className="moveset-container">
@@ -66,7 +68,7 @@ function App() {
                     {' '}
                     {move.version_group_details[0].level_learned_at}
                     {' '}
-                    through
+                    by
                     {' '}
                     {move.version_group_details[0].move_learn_method.name}
                   </li>
